0260a1075e966bc14b0559adcd81d64b93020cf2
Ivan Malik <dertalius@gmail.com>
2019-09-29 13:45:22 +0300

    code refactoring

--- Main.java
+++ Main.java
@@ -26,20 +26,11 @@ import java.lang.reflect.Method;
 import java.util.*;
 
 public class Main {
-    private static final String PATH_TO_CONFIG = "C:\\Users\\DEDUSHKA DEDULYA\\IdeaProjects\\Task2\\src\\reflection\\config.xml";
+    private static final String PATH_TO_CONFIG = "src/resources/config.xml";
     private static final String packageToScan = "reflection";
 
     public static void main(String[] args) {
-        Set<Class<?>> classes = findAllClassesInPackages(packageToScan);
-
-        List<Object> createdObjects = new ArrayList<>();
-        for (Class<?> c : classes) {
-            if (c.isAnnotationPresent(Reflected.class)) { //check if class is annotated
-                if (isClassConfigured(c.getName())) { //check if class is in config file
-                    createdObjects.add(createInstance(c.getName())); // create object and add to list
-                }
-            }
-        }
+        List<Object> createdObjects = createAllInstances();
 
         System.out.println("Print all created objects, which class with annotation @Reflected");
         for (Object obj : createdObjects) {
@@ -47,12 +38,25 @@ public class Main {
         }
     }
 
+    private static List<Object> createAllInstances() {
+        Set<Class<?>> classes = findAllClassesInPackages(packageToScan);
+
+        List<Object> createdObjects = new ArrayList<>();
+        for (Class<?> c : classes) {
+            if (c.isAnnotationPresent(Reflected.class)) {
+                if (isClassConfigured(c.getName())) {
+                    createdObjects.add(createInstance(c.getName()));
+                }
+            }
+        }
+        return createdObjects;
+    }
+
     private static Set<Class<?>> findAllClassesInPackages(String packageToScan) {
         List<ClassLoader> classLoadersList = new LinkedList<>();
         classLoadersList.add(ClasspathHelper.contextClassLoader());
         classLoadersList.add(ClasspathHelper.staticClassLoader());
 
-        //use Reflections lib to find all classes in all packages
         Reflections reflections = new Reflections(new ConfigurationBuilder()
                 .setScanners(new SubTypesScanner(false), new ResourcesScanner())
                 .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
@@ -63,11 +67,7 @@ public class Main {
 
     private static boolean isClassConfigured(String className) {
         try {
-            File fXmlFile = new File(PATH_TO_CONFIG); // use static variable
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
+            Document doc = getDocument();
 
             NodeList nList = doc.getElementsByTagName("bean");
             for (int temp = 0; temp < nList.getLength(); temp++) {
@@ -91,71 +91,84 @@ public class Main {
         Class<?> classOfCreatedObject;
         Object objectToCreate = null;
         try {
-            File inputFile = new File(PATH_TO_CONFIG);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document document = dBuilder.parse(inputFile);
-            document.getDocumentElement().normalize();
-
+            Document document = getDocument();
             NodeList nodeBeanList = document.getElementsByTagName("bean");
-
-            for (int temp = 0; temp < nodeBeanList.getLength(); temp++) {
-                Node node = nodeBeanList.item(temp);
-
+            for (int currentBean = 0; currentBean < nodeBeanList.getLength(); currentBean++) {
+                Node node = nodeBeanList.item(currentBean);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) node;
                     classOfCreatedObject = Class.forName(eElement.getAttribute("class"));
-
-                    Table<String, String, String> table = HashBasedTable.create(); //use google.common.collect.Table, like Map but for 3 vars
                     if (eElement.getAttribute("class").endsWith(className)) {
                         objectToCreate = classOfCreatedObject.newInstance();
                         Method[] methods = classOfCreatedObject.getMethods();
-
                         NodeList properties = eElement.getElementsByTagName("property");
-                        for (int i = 0; i < properties.getLength(); i++) {
-                            Node property = properties.item(i);
-                            Element eElementP = (Element) property;
-                            table.put(eElementP.getAttribute("name"), eElementP.getAttribute("value"),
-                                    eElementP.getAttribute("type"));
-                        }
-
-                        Map<String, Map<String, String>> map = table.rowMap();
-                        // iterate table, retrieve values and initialize object through setters
-                        for (Map.Entry<String, Map<String, String>> outer : map.entrySet()) {
-                            for (Map.Entry<String, String> inner : outer.getValue().entrySet()) {
-                                for (Method m : methods) {
-                                    if (m.getName().equals("set".concat(StringUtils.capitalize(outer.getKey())))) { //get setter for files. e.g field "name" - method "setName"
-                                        parseParametersAndInvoke(objectToCreate, inner.getValue(), m, inner.getKey());
-                                    }
-                                }
-                            }
-                        }
+                        Table<String, String, String> table = HashBasedTable.create();
+                        putAttributesToTable(table, properties);
+                        iterateTable(objectToCreate, methods, table.rowMap());
                     }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return objectToCreate;
     }
 
-    // pass "type" from xml config file, parse to primitive and invoke setter
+    private static Document getDocument() throws ParserConfigurationException, SAXException, IOException {
+        File inputFile = new File(PATH_TO_CONFIG);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document document = dBuilder.parse(inputFile);
+        document.getDocumentElement().normalize();
+        return document;
+    }
+
+    private static void putAttributesToTable(Table<String, String, String> table, NodeList properties) {
+        for (int i = 0; i < properties.getLength(); i++) {
+            Node property = properties.item(i);
+            Element eElementP = (Element) property;
+            table.put(eElementP.getAttribute("name"), eElementP.getAttribute("value"),
+                    eElementP.getAttribute("type"));
+        }
+    }
+
+    private static void iterateTable(Object objectToCreate, Method[] methods, Map<String, Map<String, String>> map) {
+        for (Map.Entry<String, Map<String, String>> outer : map.entrySet()) {
+            for (Map.Entry<String, String> inner : outer.getValue().entrySet()) {
+                for (Method m : methods) {
+                    if (m.getName().equals("set".concat(StringUtils.capitalize(outer.getKey())))) {
+                        parseParametersAndInvoke(objectToCreate, inner.getValue(), m, inner.getKey());
+                    }
+                }
+            }
+        }
+    }
+
     private static void parseParametersAndInvoke (Object invoker, String type, Method m, String valueToParse) {
         try {
-            if (type.equals("double") || type.equals("java.lang.Double")) {
-                m.invoke(invoker, Double.parseDouble(valueToParse));
-            } else if (type.equals("int") || type.equals("java.lang.Integer")) {
-                m.invoke(invoker, Integer.parseInt(valueToParse));
-            } else if (type.equals("long") || type.equals("java.lang.Long")) {
-                m.invoke(invoker, Long.parseLong(valueToParse));
-            } else if (type.equals("boolean") || type.equals("java.lang.Boolean")) {
-                m.invoke(invoker, Boolean.parseBoolean(valueToParse));
-            } else
-                m.invoke(invoker, valueToParse); //if type of parameter is java.lang.String
+            switch (type) {
+                case "double":
+                case "java.lang.Double":
+                    m.invoke(invoker, Double.parseDouble(valueToParse));
+                    break;
+                case "int":
+                case "java.lang.Integer":
+                    m.invoke(invoker, Integer.parseInt(valueToParse));
+                    break;
+                case "long":
+                case "java.lang.Long":
+                    m.invoke(invoker, Long.parseLong(valueToParse));
+                    break;
+                case "boolean":
+                case "java.lang.Boolean":
+                    m.invoke(invoker, Boolean.parseBoolean(valueToParse));
+                    break;
+                default:
+                    m.invoke(invoker, valueToParse);
+                    break;
+            }
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
-
 }
